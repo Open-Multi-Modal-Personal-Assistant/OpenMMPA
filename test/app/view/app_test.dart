452c8eb278adc167aba0455c5fd6cdf2c9bd137a
MrCsabaToth <csaba.toth.us@gmail.com>
2024-06-15 23:18:41 -0700

    Attempt to fix App start test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:inspector_gadget/app/app.dart';
-import 'package:inspector_gadget/counter/counter.dart';
+import 'package:inspector_gadget/main/main.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders MainPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(MainPage), findsOneWidget);
     });
   });
 }
