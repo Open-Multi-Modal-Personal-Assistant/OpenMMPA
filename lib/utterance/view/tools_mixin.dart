9d860ef3dae15eb20fb045aed3457ff7211a1772
MrCsabaToth <csaba.toth.us@gmail.com>
2024-07-01 12:48:02 -0700

    Adding web search tool based on DuckDuckGo API See https://stackoverflow.com/questions/37012469/duckduckgo-api-getting-search-results

--- lib/utterance/view/tools_mixin.dart
+++ lib/utterance/view/tools_mixin.dart
@@ -84,6 +84,21 @@ mixin ToolsMixin {
               requiredProperties: ['currencyFrom', 'currencyTo'],
             ),
           ),
+          FunctionDeclaration(
+            'webSearch',
+            'Search the web and wikipedia for facts about any topic or '
+                'gather munition to answer any questions',
+            Schema(
+              SchemaType.string,
+              properties: {
+                'query': Schema.string(
+                  description: 'The web search query or question need to be '
+                      'researched or answered',
+                ),
+              },
+              requiredProperties: ['query'],
+            ),
+          ),
         ],
       ),
     ];
@@ -111,6 +126,9 @@ mixin ToolsMixin {
           'exchangeRate': await _fetchCurrencyExchangeRate(
             CurrencyRequest.fromJson(call.args),
           ),
+        },
+      'webSearch' => {
+          'query': await _webSearch(call.args),
         },
       _ => throw UnimplementedError('Function not implemented: ${call.name}')
     };
@@ -174,5 +192,52 @@ mixin ToolsMixin {
     }
 
     return 0.0;
+  }
+
+  Future<String> _webSearch(Map<String, Object?> jsonObject) async {
+    final query = (jsonObject['query'] ?? '') as String;
+    if (query.isNullOrWhiteSpace) {
+      return 'N/A';
+    }
+
+    // https://stackoverflow.com/questions/37012469/duckduckgo-api-getting-search-results
+    const duckDuckGoBaseUrl = 'http://api.duckduckgo.com';
+    final duckDuckGoUrl = Uri.http(duckDuckGoBaseUrl, '/', {
+      'q': query,
+      'format': 'json',
+      'no_html': 1,
+      'skip_disambig': 1,
+    });
+
+    var result = '';
+    final searchResult = await http.get(duckDuckGoUrl);
+    if (searchResult.statusCode == 200) {
+      final resultJson = json.decode(searchResult.body) as Map<String, dynamic>;
+      if (resultJson.containsKey('AbstractText')) {
+        result = resultJson['AbstractText'] as String;
+      } else if (resultJson.containsKey('Abstract')) {
+        result = resultJson['Abstract'] as String;
+      }
+
+      if (resultJson.containsKey('AbstractSource') ||
+          resultJson.containsKey('AbstractURL')) {
+        result += ' (source: ';
+        if (resultJson.containsKey('AbstractSource')) {
+          result += resultJson.containsKey('AbstractSource') as String;
+        }
+
+        if (resultJson.containsKey('AbstractURL')) {
+          if (resultJson.containsKey('AbstractSource')) {
+            result += ', url: ';
+          }
+
+          result += resultJson.containsKey('AbstractURL') as String;
+        }
+
+        result += ')';
+      }
+    }
+
+    return result;
   }
 }
