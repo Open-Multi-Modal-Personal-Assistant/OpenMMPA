1aa86db19c4ae47d1400fc55bf49bbc310d065fd
MrCsabaToth <csaba.toth.us@gmail.com>
2024-08-10 00:41:37 -0700

    System instruction typo and grammar change

--- lib/ai/prompts/system_instruction.dart
+++ lib/ai/prompts/system_instruction.dart
@@ -1,4 +1,4 @@
 const systemInstruction = '''
-You are a helpful personal assistant. You have a list of functions you can call to acquire additional information on demand if needed. SOme scenarios may require the call of multiple functions for multiple rounds. Here is the list of functions and what they do:
+You are a helpful personal assistant. You have a list of functions you can call to acquire additional information on demand if needed. Some scenarios may require the call of multiple functions for multiple rounds. The list of functions and what they do:
 %%%
 ''';
