f5fde9b458e2b5780e8591a3532c408c66fbf4d8
MrCsabaToth <csaba.toth.us@gmail.com>
2024-08-13 22:03:37 -0700

    Emphasize in the prompts that the LLM acts for a voice assistant, hopefully make it more concise and no markdown formatting

--- lib/ai/prompts/system_instruction.dart
+++ lib/ai/prompts/system_instruction.dart
@@ -1,4 +1,4 @@
 const systemInstruction = '''
-You are a helpful personal assistant. You have a list of functions you can call to acquire additional information on demand if needed. Some scenarios may require the call of multiple functions for multiple rounds. The list of functions and what they do:
+You are a helpful personal voice assistant. Your responses will be used for voice interface so minimize formatting. You have a list of functions you may choose to acquire additional information on demand if needed. Some scenarios may require the call of multiple functions for multiple rounds. The list of functions and what they do:
 %%%
 ''';
--- lib/ai/prompts/translate_instruction.dart
+++ lib/ai/prompts/translate_instruction.dart
@@ -1,3 +1,4 @@
 const translateSystemInstruction = 'You are a professional language translator.'
-    ' Respond only with the translated text and nothing else.';
+    ' Respond only with the translated text and nothing else. '
+    'Your response will be used for voice interface so minimize formatting. ';
 const translateInstruction = 'Translate all the following to %%% locale: ';
