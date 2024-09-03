225dfbdf925cebaf5d8736ba36dd7be77bba6305
MrCsabaToth <csaba.toth.us@gmail.com>
2024-09-03 15:52:51 -0700

    Adjust History and Personalization RAG stuffing: remove extra new line #45

--- lib/ai/prompts/history_rag_stuffing.dart
+++ lib/ai/prompts/history_rag_stuffing.dart
@@ -2,6 +2,5 @@ const historyRagStuffingVariable = '{{HISTORY}}';
 const historyRagStuffingTemplate = '''
 The following earlier historical conversation pieces may or may not be beneficial to answer the question:
 <relatedHistory>
-$historyRagStuffingVariable
-</relatedHistory>
+$historyRagStuffingVariable</relatedHistory>
 ''';
--- lib/ai/prompts/personalization_rag_stuffing.dart
+++ lib/ai/prompts/personalization_rag_stuffing.dart
@@ -2,6 +2,5 @@ const p13nRagStuffingVariable = '{{PERSONALIZATION}}';
 const p13nStuffingTemplate = '''
 The following personal facts might or may be beneficial to answer the question:
 <personalFacts>
-$p13nRagStuffingVariable
-</personalFacts>
+$p13nRagStuffingVariable</personalFacts>
 ''';
