58cb75d8bbf4707beaab1526f7d9cd881d1e12ac
MrCsabaToth <csaba.toth.us@gmail.com>
2024-09-29 21:55:13 -0700

    Trying to work around the new stable models cautious (lame?) behavior about location and time #53 #56

--- lib/ai/prompts/system_instruction.dart
+++ lib/ai/prompts/system_instruction.dart
@@ -6,4 +6,7 @@ const systemInstructionTemplate =
     'You have a list of functions you can choose to '
     'retrieve additional information when needed. '
     'Multiple rounds might be needed to call those functions. '
-    'If unsure the web research tool can retrieve all kinds of information.';
+    'If unsure the web research tool can retrieve all kinds of information. '
+    'If a request refers to the future and the location has relevance '
+    'and it is not specified, then feel free to assume the current location '
+    'of the user (which will be given in the personal facts section).';
