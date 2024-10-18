de89be9f8fb743700dfbcc6c4c70c711dcbaac2b
MrCsabaToth <csaba.toth.us@gmail.com>
2024-10-17 22:34:31 -0700

    Adding multi modal (for image and video) and multi lingual (for text) embedding function #48

---
+++ functions/fn_impl/embed.py
@@ -0,0 +1,104 @@
+from firebase_functions import https_fn
+from firebase_admin import initialize_app, storage
+import firebase_admin
+import json
+import vertexai
+
+from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
+from vertexai.vision_models import Image as VMImage
+from vertexai.vision_models import MultiModalEmbeddingModel, MultiModalEmbeddingResponse
+from vertexai.vision_models import Video as VMVideo
+from vertexai.vision_models import VideoSegmentConfig
+
+
+@https_fn.on_request()
+def embed(req: https_fn.Request) -> https_fn.Response:
+    """Synthesizes speech from the input string of text or ssml.
+    Returns:
+        Encoded audio file in the body.
+    Note: ssml must be well-formed according to:
+        https://www.w3.org/TR/speech-synthesis/
+    """
+    # Set CORS headers for the preflight request
+    if req.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    if not firebase_admin._apps:
+        initialize_app()
+
+    request_json = req.get_json(silent=True)
+    request_args = req.args
+    request_form = req.form
+
+    if request_json and 'data' in request_json:
+        request_json = request_json['data']
+
+    if request_json and 'image_path' in request_json:
+        image_path = request_json['image_path']
+    elif request_args and 'image_path' in request_args:
+        image_path = request_args['image_path']
+    elif request_form and 'image_path' in request_form:
+        image_path = request_form['image_path']
+    else:
+        image_path = None
+
+    if request_json and 'video_path' in request_json:
+        video_path = request_json['video_path']
+    elif request_args and 'video_path' in request_args:
+        video_path = request_args['video_path']
+    elif request_form and 'video_path' in request_form:
+        video_path = request_form['video_path']
+    else:
+        video_path = None
+
+    if request_json and 'text' in request_json:
+        text = request_json['text']
+    elif request_args and 'text' in request_args:
+        text = request_args['text']
+    elif request_form and 'text' in request_form:
+        text = request_form['text']
+    else:
+        text = None
+
+    project_id = 'open-mmpa'
+    region = 'us-central1'
+    vertexai.init(project=project_id, location=region)
+
+    multi_modal_embedding_model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
+
+    image = VMImage.load_from_file(image_path) if image_path else None
+    video = VMVideo.load_from_file(video_path) if video_path else None
+    dimension = 1408
+
+    embeddings = multi_modal_embedding_model.get_embeddings(
+        image=image,
+        video=video,
+        video_segment_config=VideoSegmentConfig(),
+        contextual_text=text,
+        dimension=dimension,
+    ) if image or video else []
+
+    # Multi lingual text embedding
+    # The task type for embedding. Check the available tasks in the model's documentation.
+    task = "RETRIEVAL_DOCUMENT"
+    multi_lingual_embedding_model = TextEmbeddingModel.from_pretrained("text-embedding-004")
+    inputs = [TextEmbeddingInput(text, task)]
+    text_embeddings = multi_lingual_embedding_model.get_embeddings(inputs, {})
+    embeddings.extend(text_embeddings)
+
+    embedding_values = [embedding.values for embedding in embeddings]
+
+    return https_fn.Response(
+        json.dumps(dict(data=embedding_values)),
+        status=200,
+        content_type='application/json',
+    )
