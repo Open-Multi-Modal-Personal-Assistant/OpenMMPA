4e5aaf8371a05444f3304caf0ac4cde612abb3ab
MrCsabaToth <csaba.toth.us@gmail.com>
2024-10-19 21:25:50 -0700

    Fixing embedding function return call handling #48

--- lib/ai/service/embedding_list.dart
+++ lib/ai/service/embedding_list.dart
@@ -6,33 +6,36 @@ class Embeddings {
   Embeddings.fromJson(Map<String, Object?> embeddingMap) {
     textEmbeddings.clear();
     if (embeddingMap.containsKey('text') && embeddingMap['text'] != null) {
-      final embeddingsList = embeddingMap['text']! as List<Object?>;
+      final embeddingsList = embeddingMap['text']! as List<dynamic>;
       for (final embeddings in embeddingsList.nonNulls) {
-        final embedding = (embeddings as List<Object?>).nonNulls;
+        final embedding =
+            (embeddings as List<dynamic>).nonNulls.toList(growable: false);
         if (embedding.isNotEmpty) {
-          textEmbeddings
-              .add(dimensionalityReduction(embedding as List<double>));
+          textEmbeddings.add(dimensionalityReduction(embedding.cast<double>()));
         }
       }
     }
 
     imageEmbeddings.clear();
     if (embeddingMap.containsKey('image') && embeddingMap['image'] != null) {
-      final embedding = (embeddingMap['image']! as List<Object?>).nonNulls;
+      final embedding = (embeddingMap['image']! as List<dynamic>)
+          .nonNulls
+          .toList(growable: false);
       if (embedding.isNotEmpty) {
         imageEmbeddings
-            .addAll(dimensionalityReduction(embedding as List<double>));
+            .addAll(dimensionalityReduction(embedding.cast<double>()));
       }
     }
 
     videoEmbeddings.clear();
     if (embeddingMap.containsKey('video') && embeddingMap['video'] != null) {
-      final embeddingsList = embeddingMap['video']! as List<Object?>;
+      final embeddingsList = embeddingMap['video']! as List<dynamic>;
       for (final embeddings in embeddingsList.nonNulls) {
-        final embedding = (embeddings as List<Object?>).nonNulls;
+        final embedding =
+            (embeddings as List<dynamic>).nonNulls.toList(growable: false);
         if (embedding.isNotEmpty) {
           videoEmbeddings
-              .add(dimensionalityReduction(embedding as List<double>));
+              .add(dimensionalityReduction(embedding.cast<double>()));
         }
       }
     }
